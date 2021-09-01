e660f61a8578a5aef20463eb57a9201693cead45
Estanislao Bel <estanislao.bel@alumnos.frgp.utn.edu.ar>
2021-09-01 11:37:43 -0300

    Se agrega constructor y atributos

--- src/figuras/rectangulo.java
+++ src/figuras/rectangulo.java
@@ -1,5 +1,12 @@
 package figuras;
 
 public class rectangulo {
+	private double ancho;
+	private double alto;
+
+	public rectangulo(double ancho, double alto) {
+		this.ancho=ancho;
+		this.alto=alto;
+	}
 
 }
